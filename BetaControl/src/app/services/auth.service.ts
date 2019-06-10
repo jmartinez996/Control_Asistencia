d88d4aed950a7a1269743139999048687c2726b3
jmartinez996 <jmartinez2015@alu.uct.cl>
2019-06-09 23:29:01 -0400

    token mejorado
    asd

--- BetaControl/src/app/services/auth.service.ts
+++ BetaControl/src/app/services/auth.service.ts
@@ -56,6 +56,9 @@ export class AuthService {
   private guardartoke(idToken: string) {
     this.userToken = idToken;
     localStorage.setItem('token', idToken);
+    let hoy = new Date();
+    hoy.setSeconds( 3600 );
+    localStorage.setItem('expira', hoy.getTime().toString());
   }
   leerToken() {
     if (localStorage.getItem('token')) {
@@ -66,6 +69,16 @@ export class AuthService {
     return this.userToken;
   }
   estaAutenticado(): boolean {
-    return this.userToken.length > 2;
+    if ( this.userToken.length < 2 ){
+      return false;
+    }
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime( expira );
+    if ( expiraDate > new Date() ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
